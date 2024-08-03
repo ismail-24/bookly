9dc16f7aaccc0d8ea53fdc15522036f71951ed54
ismail-24 <ismailtarek888@gmail.com>
2024-08-03 19:42:29 +0300

    splash view widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,7 +13,7 @@ class Bookly extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: SplashView(),
     );
   }
 }
