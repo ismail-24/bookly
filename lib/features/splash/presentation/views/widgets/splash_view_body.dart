15da3810adbfda47e3f04c6d6a361dd1e48fee29
ismail-24 <ismailtarek888@gmail.com>
2024-08-04 21:11:15 +0300

    change theme & add text to splash view body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -14,6 +14,11 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          style: TextStyle(),
+        ),
       ],
     );
   }
