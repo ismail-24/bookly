0e17da1ed6fd790b128011f506643ea393f15069
ismail-24 <ismailtarek888@gmail.com>
2024-09-18 14:34:06 +0300

    add rating to book details

--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -4,15 +4,18 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
           color: Color(0xffFFDD4F),
+          size: 14,
         ),
         SizedBox(width: 6.3.w),
         Text(
@@ -20,13 +23,9 @@ class BookRating extends StatelessWidget {
           style: Styles.textStyle16,
         ),
         SizedBox(width: 5.w),
-        Text(
-          '(12000)',
-          style: Styles.textStyle14.copyWith(
-            color: const Color(
-              0xff707070,
-            ),
-          ),
+        Opacity(
+          opacity: 0.5,
+          child: Text('(12000)', style: Styles.textStyle14),
         ),
       ],
     );
