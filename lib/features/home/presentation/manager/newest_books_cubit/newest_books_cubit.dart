c613f6fe426fc271b827bfa7d0505b1f6b208b76
ismail-24 <ismailtarek888@gmail.com>
2024-10-02 12:30:31 +0300

    newest books cubit done

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -7,5 +7,25 @@ import '../../../data/models/book_model/book_model.dart';
 part 'newest_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewestBooksState> {
-  NewsetBooksCubit() : super(NewestBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(NewestBooksLoading());
+
+    var result = await homeRepo.fetchNewestBooks();
+
+    result.fold(
+      (failure) {
+        emit(
+          NewestBooksFailure(failure.errMessage),
+        );
+      },
+      (books) {
+        emit(
+          NewestBooksSuccess(books),
+        );
+      },
+    );
+  }
 }
