ad084883bf885b085a684eee7afb48a38a7b552e
ismail-24 <ismailtarek888@gmail.com>
2024-10-14 11:20:02 +0300

    cached network image

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookItem extends StatelessWidget {
@@ -12,17 +13,17 @@ class CustomBookItem extends StatelessWidget {
     // final double screenHeight = MediaQuery.of(context).size.height;
     // final double screenWidth = MediaQuery.of(context).size.width;
 
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-              imageUrl,
-            ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
+            size: 60,
           ),
+          fit: BoxFit.fill,
         ),
       ),
     );
